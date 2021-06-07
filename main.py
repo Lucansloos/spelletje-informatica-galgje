4144ad4f6334cdb0a6b7c5efb4cb0bddfa272d5c
Lucas Teunisse <>
2021-06-07 09:55:56 +0000

    naam en woorden

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ naam = input ('Hoe heet je? ')
 print ()
 print ('Hello,', naam,'tijd om galgje te spelen ')
 print ()
+
+woorden = ["informatica","informatiekunde","spelletje",
+"aardigheidje","scholier","fotografie","waardebepaling","specialiteit","verzekering","universiteit","heesterperk"]
