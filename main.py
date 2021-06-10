d65af44495dfe63158cfd08875930588625d6860
Lucas Teunisse <74767445+Lucansloos@users.noreply.github.com>
2021-06-10 16:50:19 +0000

    fixen van bug door alle "print" onder elkaar te zetten

--- main.py
+++ main.py
@@ -35,12 +35,29 @@ turns = 10
 while turns > 0:
 
   letters_not_guesed = 0             
-    toonwoord=''
-    for char in woord: 
-        if char in guesses:  
-           toonwoord=toonwoord + (char)
-        else:
-           toonwoord=toonwoord + ('_') 
-           letters_not_guesed += 1   
-     print ('Woord:',toonwoord, '. Al gegokte letters:', guesses)      
-     print ()
+  toonwoord=''
+  for char in woord: 
+      if char in guesses:  
+          toonwoord=toonwoord + (char)
+      else:
+          toonwoord=toonwoord + ('_') 
+          letters_not_guesed += 1   
+  print ('Woord:',toonwoord, '. Al gegokte letters:', guesses)      
+  print ()
+
+
+
+
+
+
+
+
+
+     
+     
+     
+     
+     
+     
+     
+  
