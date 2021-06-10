a042a0ccb52c32301cd3dc5d367049cbe706a98b
Lucas Teunisse <74767445+Lucansloos@users.noreply.github.com>
2021-06-10 16:28:42 +0000

    woord kiezen en woord raden

--- main.py
+++ main.py
@@ -26,3 +26,20 @@ time.sleep(1)
 
 print ('We gaan beginnen ...')
 time.sleep(0.5)
+
+woordkiezen = random.choice(woorden)
+woord = (woordkiezen) 
+
+guesses = ''
+turns = 10
+while turns > 0:
+
+  letters_not_guesed = 0             
+    toonwoord=''
+    for char in woord: 
+        if char in guesses:  
+           toonwoord=toonwoord + (char)
+        else:
+           toonwoord=toonwoord + ('_') 
+           letters_not_guesed += 1   
+     print ('Woord:',toonwoord, '. Al gegokte letters:', guesses)      
